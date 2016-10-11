df0f6639f5f6cb9e8f2f259adb0cab2208bf24da
Fausto Napoli <worsearoom@gmail.com>
2016-10-12 00:16:04 +0200

    - First commit

---
+++ desktop/src/net/nothuman/nhglib/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.nothuman.nhglib.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.nothuman.nhglib.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
---
+++ html/src/net/nothuman/nhglib/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.nothuman.nhglib.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.nothuman.nhglib.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Main();
+        }
+}
