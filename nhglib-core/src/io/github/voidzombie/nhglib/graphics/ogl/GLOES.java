fadce6ea3b6348fdeea14071e330264538e23595
Fausto Napoli <worsearoom@gmail.com>
2017-08-08 01:43:50 +0200

    - Custom Framebuffer

---
+++ nhglib-core/src/io/github/voidzombie/nhglib/graphics/ogl/GLOES.java
@@ -0,0 +1,7 @@
+package io.github.voidzombie.nhglib.graphics.ogl;
+
+public class GLOES {
+    public final static int GL_DEPTH_COMPONENT24_OES = 0x81a6;
+    public final static int GL_DEPTH_COMPONENT32_OES = 0x81a7;
+    public final static int GL_FLOAT = 0x1406;
+}
