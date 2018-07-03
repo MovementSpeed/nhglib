3b0abf144316c9898b294e634e634aa10bc85ad8
fnapoli <fausto.napoli@sparkling18.com>
2018-07-04 01:55:41 +0200

    -

--- nhglib-core/src/io/github/movementspeed/nhglib/files/gltf/jgltf/model/impl/creation/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Classes for creating {@link io.github.movementspeed.nhglib.files.gltf.jgltf.model.GltfModel} structures.
- * <br>
- * These classes are highly preliminary, and should not be considered to
- * be part of the public API.
- */
-package io.github.movementspeed.nhglib.files.gltf.jgltf.model.impl.creation;
-
--- nhglib-core/src/io/github/movementspeed/nhglib/files/gltf/jgltf/model/io/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes for reading and writing glTF data
- */
-package io.github.movementspeed.nhglib.files.gltf.jgltf.model.io;
-
